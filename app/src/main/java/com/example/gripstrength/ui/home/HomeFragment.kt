810bb58d12e5829b712a160ced6aae0537e8fda0
Rasesepa Takalani <takalani280@gmail.com>
2023-09-20 13:53:10 +0200

    bluetooth functionality

--- app/src/main/java/com/example/gripstrength/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/gripstrength/ui/home/HomeFragment.kt
@@ -1,17 +1,84 @@
 package com.example.gripstrength.ui.home
 
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothSocket
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.gripstrength.databinding.FragmentHomeBinding
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.UUID
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+
+    //Get the bluetooth adapter
+    val REQUEST_ENABLE_BT = 1
+    private val REQUEST_BLUETOOTH_PERMISSION = 1
+    private var bluetoothSocket: BluetoothSocket? = null
+    private var inputStream: InputStream? = null
+    private var outputStream: OutputStream? = null
+    private var deviceAddress: String = ""
+    private var device_found = false
+    private var connectSuccessful = false
+    private var connectToDevice = ConnectToDevice()
+    private lateinit var myBluetoothService: MyBluetoothService
+    private var maxGrip = 0
+
+    //Defining states
+    private val findBluetoothModule = "find"
+    private var stateOfBluetooth = findBluetoothModule
+    private val readyToConnect = "ready"
+    private val store = "store"
+    private val ledOff = "off"
+    private val measuring = "measure"
+
+    //initialize handler
+    val handler = Handler(Looper.getMainLooper()) { msg ->
+        when (msg.what) {
+            MESSAGE_READ -> {
+                val numBytes = msg.arg1
+                val message = String(msg.obj as ByteArray, 0, numBytes)
+
+                // Update the TextView with the received message
+                binding.textHome.text = "$message N"
+
+            }
+            MESSAGE_TOAST ->{
+                binding.textHome.text = "data toast"
+            }
+            MESSAGE_WRITE -> {
+                binding.textHome.text = "data write"
+            }
+
+            else -> {
+                // Display a message when there's no specific message to handle
+                binding.textHome.text = "No message to display"
+            }
+        }
+        true
+    }
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,10 +95,76 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        //Checking if bluetooth is supported on this device
+        val bluetoothManager = context?.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            val textView: TextView = binding.textHome
+            homeViewModel.text.observe(viewLifecycleOwner) {
+                textView.text = "Bluetooth not supported"
+            }
         }
+
+        //Check if bluetooth is enabled, if not prompt the user to enable it
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+
+        //Inform the user if the user has been enabled or not
+        fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            if(requestCode == REQUEST_ENABLE_BT){
+                if(resultCode == Activity.RESULT_OK){
+                    if(bluetoothAdapter?.isEnabled == true){
+                        Toast.makeText(requireContext(), "Bluetooth has been enabled",Toast.LENGTH_SHORT).show()
+                    }
+                    else{
+                        Toast.makeText(requireContext(), "Bluetooth has been disabled",Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                else if(resultCode == Activity.RESULT_CANCELED){
+                    Toast.makeText(requireContext(), "Bluetooth has been disabled",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        //Test if connect button is working
+        binding.connectButton.setOnClickListener{
+            pairedDeviceList()
+            when(stateOfBluetooth){
+                findBluetoothModule -> {
+                    if(device_found){
+                        binding.connectButton.text = "connect"
+                        stateOfBluetooth = readyToConnect
+                    }
+                }
+                readyToConnect ->{
+                    connectToDevice.connect()
+                    binding.connectButton.text = "measure"
+                    Toast.makeText(requireContext(), "You are connected",Toast.LENGTH_SHORT).show()
+                    stateOfBluetooth = measuring
+
+                }
+                measuring ->{
+                    binding.connectButton.text = "Store"
+                    myBluetoothService.startReadingData()
+                    myBluetoothService.stopReadingData()
+                    stateOfBluetooth = store
+                }
+                store ->{
+                    binding.textHome.text = ""
+                }
+
+                else ->{
+                    binding.connectButton.text = " "
+                }
+
+            }
+        }
+
         return root
     }
 
@@ -39,4 +172,117 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    fun pairedDeviceList() {
+        val bluetoothManager = context?.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
+
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.BLUETOOTH
+            ) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.BLUETOOTH_ADMIN
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Request the missing Bluetooth permissions here
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN
+                ),
+                REQUEST_BLUETOOTH_PERMISSION
+            )
+        } else {
+            // Permission is already granted; you can proceed with Bluetooth functionality
+            //binding.textHome.text = "Bluetooth permission is already granted"
+            device_found = false
+            val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+            pairedDevices?.forEach { device ->
+                val deviceName = device.name
+                val deviceHardwareAddress = device.address // MAC address
+                if(device.name == "HC-05"){
+                    device_found = true
+                    deviceAddress = device.address
+                }
+                binding.textHome.text = "$deviceName has address $deviceHardwareAddress"
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_BLUETOOTH_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, you can proceed with Bluetooth functionality here
+                binding.textHome.text = "Bluetooth permission granted"
+            } else {
+                // Permission denied, handle accordingly
+                binding.textHome.text = "Bluetooth permission denied"
+            }
+        }
+    }
+
+    private inner class ConnectToDevice() {
+        fun connect() {
+            val bluetoothManager = context?.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+            val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
+            val device: BluetoothDevice? = bluetoothAdapter?.getRemoteDevice(deviceAddress)
+
+            Thread {
+                // Check for Bluetooth permissions here if needed
+
+                try {
+                    if (ContextCompat.checkSelfPermission(
+                            requireContext(),
+                            Manifest.permission.BLUETOOTH
+                        ) != PackageManager.PERMISSION_GRANTED ||
+                        ContextCompat.checkSelfPermission(
+                            requireContext(),
+                            Manifest.permission.BLUETOOTH_ADMIN
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        binding.textHome.text = "Permission was not granted when trying to establish connection"
+                    }
+
+                    else{
+                        bluetoothSocket = device?.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
+                        // Connect to the device
+                        bluetoothSocket?.connect()
+                        connectSuccessful = true
+
+                        // Get input and output streams
+                        inputStream = bluetoothSocket?.inputStream
+                        outputStream = bluetoothSocket?.outputStream
+                        //initialize bluetooth data reader
+                        myBluetoothService = MyBluetoothService(handler, bluetoothSocket)
+                    }
+
+                    // Perform Bluetooth communication, read, write data, etc.
+
+                } catch (e: IOException) {
+                    // Handle connection error
+                    //binding.textHome.text = "You are not really connected"
+                    Log.e(TAG, "Could not open the socket", e)
+                    e.printStackTrace()
+                    // You may want to handle this error and possibly notify the user
+                }
+            }.start()
+        }
+
+        fun disconnect() {
+            try {
+                bluetoothSocket?.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/gripstrength/ui/home/MyBluetoothService.kt
@@ -0,0 +1,126 @@
+package com.example.gripstrength.ui.home
+
+import android.bluetooth.BluetoothSocket
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+
+private const val TAG = "MY_APP_DEBUG_TAG"
+
+// Defines several constants used when transmitting messages between the
+// service and the UI.
+const val MESSAGE_READ: Int = 0
+const val MESSAGE_WRITE: Int = 1
+const val MESSAGE_TOAST: Int = 2
+// ... (Add other message types here as needed.)
+
+class MyBluetoothService(
+    // handler that gets info from Bluetooth service
+    private val handler: Handler,
+    private val bluetoothSocket: BluetoothSocket? // Pass the BluetoothSocket as nullable
+) {
+    private val mmBuffer: ByteArray = ByteArray(1024) // mmBuffer store for the stream
+    private var isDone = false
+    private inner class ConnectedThread : Thread() {
+
+        override fun run() {
+            var numBytes: Int // bytes returned from read()
+
+            // Check if the BluetoothSocket is null
+            if (bluetoothSocket == null) {
+                Log.e(TAG, "BluetoothSocket is null, cannot establish connection.")
+                return
+            }
+
+            // Use the BluetoothSocket's input stream
+            val mmInStream: InputStream
+            try {
+                mmInStream = bluetoothSocket.inputStream
+            } catch (e: IOException) {
+                Log.e(TAG, "Error opening input stream", e)
+                return
+            }
+
+            // Keep listening to the InputStream until an exception occurs.
+            val startTimeMillis = System.currentTimeMillis()
+            val durationMillis = 10000 // Set the desired duration in milliseconds (e.g., 5000 ms or 5 seconds)
+            //while (System.currentTimeMillis() - startTimeMillis < durationMillis) {
+
+            while (true) {
+                // Read from the InputStream.
+                numBytes = try {
+                    Log.d(TAG, "numBytes")
+                    mmInStream.read(mmBuffer)
+                } catch (e: IOException) {
+                    Log.d(TAG, "Input stream was disconnected", e)
+                    break
+                }
+
+                // Ensure that numBytes and mmBuffer are correct before sending the message
+                Log.d(TAG, "numBytes: $numBytes")
+                Log.d(TAG, "mmBuffer: ${String(mmBuffer, 0, numBytes)}")
+
+                val readMsg = handler.obtainMessage(
+                    MESSAGE_READ, numBytes, -1,
+                    mmBuffer
+                )
+                readMsg.sendToTarget()
+            }
+            if(System.currentTimeMillis() - startTimeMillis > durationMillis){isDone = true}
+        }
+    }
+
+    // Call this from the main activity to send data to the remote device.
+    fun write(bytes: ByteArray) {
+        if (bluetoothSocket == null) {
+            Log.e(TAG, "BluetoothSocket is null, cannot establish connection.")
+            return
+        }
+        val mmOutStream: OutputStream
+        try {
+            mmOutStream = bluetoothSocket.outputStream
+        } catch (e: IOException) {
+            Log.e(TAG, "Error opening input stream", e)
+            return
+        }
+
+        try {
+            mmOutStream.write(bytes)
+        } catch (e: IOException) {
+            Log.e(TAG, "Error occurred when sending data", e)
+
+            // Send a failure message back to the activity.
+            val writeErrorMsg = handler.obtainMessage(MESSAGE_TOAST)
+            val bundle = Bundle().apply {
+                putString("toast", "Couldn't send data to the other device")
+            }
+            writeErrorMsg.data = bundle
+            handler.sendMessage(writeErrorMsg)
+            return
+        }
+
+        // Share the sent message with the UI activity.
+        val writtenMsg = handler.obtainMessage(
+            MESSAGE_WRITE, -1, -1, mmBuffer)
+        writtenMsg.sendToTarget()
+    }
+    // Create an instance of ConnectedThread and start it
+    private val connectedThread: ConnectedThread = ConnectedThread()
+
+    // Start reading data when needed
+    fun startReadingData() {
+        connectedThread.start()
+    }
+
+    // Stop reading data when needed
+    fun stopReadingData() {
+        connectedThread.interrupt()
+    }
+
+    fun isDoneReading(): Boolean{
+        return isDone
+    }
+}
